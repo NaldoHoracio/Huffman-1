0015594fc62dd26408f32a4855769e0529eeaed5
NaldoHoracio <edvonaldo.horacio@hotmail.com>
2016-10-16 13:34:00 -0300

    Update maptree.c

--- maptree.c
+++ maptree.c
@@ -3,6 +3,7 @@
 #include "priorityQueue.h"
 #include "hash_table.h"
 #include "frequencyCounter.h"
+#include "maptree.h"
 
 
 /*main()
@@ -12,7 +13,7 @@
     Node_String* binary_value = create_linked_list(); //criando a lista encadeada binary_value
 }
 */
-void maptree(Node* current, Node_String* representree, Node_String* binary_value, Hashtable hashtree)	//função recebe um ponteiro e uma string
+void maptree(Node* current, Node_String* representree, Node_String* binary_value, Hashtable *hashtree)	//função recebe um ponteiro e uma string
 {
     if(current-> p_left == NULL && current-> p_right == NULL){ 		//se (o ponteiro aponta pra uma folha)
         if(current->n_character == '*'|| current->n_character == '!'){	//se o caracter for '*' ou '!'
@@ -25,10 +26,10 @@ void maptree(Node* current, Node_String* representree, Node_String* binary_value
 
     else{							//se não é folha, então é representado por '*' na árvore
         representree = insert_node(representree, '*');
-        Node_String* left =insert_node(binary_value, '0');
-        Node_String* right =insert_node(binary_value, '1');
-        maptree(current-> p_left, representree, left);	//percorre a árvore a esquerda e add '0'
-        maptree(current-> p_right, representree, right);//percorre a árvore a direita e add '1'
+        Node_String* left = insert_node(binary_value, '0');
+        Node_String* right = insert_node(binary_value, '1');
+        maptree(current-> p_left, representree, left, hashtree);	//percorre a árvore a esquerda e add '0'
+        maptree(current-> p_right, representree, right, hashtree);//percorre a árvore a direita e add '1'
     }
 }
 
