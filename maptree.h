aea9b3534da56c18faa7bc6f92f73dee0d7ae204
NaldoHoracio <edvonaldo.horacio@hotmail.com>
2016-10-16 13:35:33 -0300

    Update maptree.h

--- maptree.h
+++ maptree.h
@@ -1,4 +1,4 @@
-//
+///
 // Created by alvino on 15/10/16.
 //
 
@@ -9,6 +9,6 @@
 #include "hash_table.h"
 #include "frequencyCounter.h"
 
-void maptree(Node* current, Node_String* representree, Node_String* binary_value, Hashtable hashtree);
+void maptree(Node* current, Node_String* representree, Node_String* binary_value, Hashtable *hashtree);
 
 #endif //STRING_MAPTREE_H
