6b50a1e6fae815902b65dfdf552080a2cfe079ae
NaldoHoracio <edvonaldo.horacio@hotmail.com>
2016-10-16 21:56:49 -0300

    Create helpful_function.h

---
+++ helpful_function.h
@@ -0,0 +1,12 @@
+//
+// Created by edvonaldoh on 30/09/16.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int is_bit_i_set(unsigned char c, int i); // Verifica se o bit de posição i do caractere c está setado - tem valor 1
+
+unsigned char set_bit(unsigned char c, int i); // Seta o bit da posição i do caractere c
+
