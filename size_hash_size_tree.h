3d99f712bd53657e8fe0a0ac8f9ad649590d2162
NaldoHoracio <edvonaldo.horacio@hotmail.com>
2017-01-06 11:53:33 -0300

    Update and rename size_hash_size_trash.h to size_hash_size_tree.h

---
+++ size_hash_size_tree.h
@@ -0,0 +1,16 @@
+//
+// Created by edvonaldoh on 17/10/16.
+//
+
+#ifndef SIZE_HASH_SIZE_TRASH_C
+#define SIZE_HASH_SIZE_TRASH_C
+#include <math.h>
+#include "bitVector.h"
+#include "bitVector.c"
+#include "stringList.h"
+#include "stringList.c"
+
+Node_String* size_tree_size_trash(FILE *p_file);
+
+#endif// SIZE_HASH_SIZE_TRASH_C
+
