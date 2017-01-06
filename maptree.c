a6a70a0f5306333735836b35d32352936a75588c
NaldoHoracio <edvonaldo.horacio@hotmail.com>
2017-01-06 14:33:12 -0300

    Update maptree.c

--- maptree.c
+++ maptree.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include "stringList.h"
-#include "priorityQueue.h"
-#include "hash_table.h"
-#include "frequencyCounter.h"
+//
+// Created by alvino on 03/10/16.
+//
+#ifndef MAPTREE_C
+#define MAPTREE_C
 
-
+#include "maptree.h"
 /*main()
 {
     Hashtable* hashtree = create_hash_table(); // criando a hash que receberá as chaves e valores das folhas da árvore
@@ -15,51 +15,51 @@
 //função recebe um ponteiro e uma string
 Node_String* maptreeRepresetantion(Node* current, Node_String* representree, Node_String* binary_value, Hashtable* hashtree)
 {
-    if(current-> p_left == NULL && current-> p_right == NULL){ 		//se (o ponteiro aponta pra uma folha)
-        if(current->n_character == '*'|| current->n_character == '!'){	//se o caracter for '*' ou '!'
-            representree = insert_node(representree, '!');				//lista representação da árvore recebe um marcador
-        }
-        representree = insert_node(representree, current->n_character);	//vetor representação da árvore recebe o caracter da folha
-        return representree;
-        //hashtree = put(hashtree, current-> n_character, binary_value);	//coloca na hash a chave(caracter) e valor(binário)
-    }
+	if(current-> p_left == NULL && current-> p_right == NULL){ 		//se (o ponteiro aponta pra uma folha)
+		if(current->n_character == '*'|| current->n_character == '!'){	//se o caracter for '*' ou '!'
+			representree = insert_node(representree, '!');				//lista representação da árvore recebe um marcador
+		}
+		representree = insert_node(representree, current->n_character);	//vetor representação da árvore recebe o caracter da folha
+		return representree;
+		//hashtree = put(hashtree, current-> n_character, binary_value);	//coloca na hash a chave(caracter) e valor(binário)
+	}
 
-    else{							//se não é folha, então é representado por '*' na árvore
-        representree = insert_node(representree, '*');
-        Node_String* left =insert_node(binary_value, '0');
-        Node_String* right =insert_node(binary_value, '1');
-        representree = maptreeRepresetantion(current-> p_left, representree, left, hashtree);	//percorre a árvore a esquerda e add '0'
-        representree = maptreeRepresetantion(current-> p_right, representree, right, hashtree);//percorre a árvore a direita e add '1'
-    }
-    return representree;
+	else{							//se não é folha, então é representado por '*' na árvore
+		representree = insert_node(representree, '*');
+		Node_String* left =insert_node(binary_value, '0');
+		Node_String* right =insert_node(binary_value, '1');
+		representree = maptreeRepresetantion(current-> p_left, representree, left, hashtree);	//percorre a árvore a esquerda e add '0'
+		representree = maptreeRepresetantion(current-> p_right, representree, right, hashtree);//percorre a árvore a direita e add '1'
+	}
+	return representree;
 }
 
 Hashtable* maptreeHashTable(Node* current, long int binary_value, Hashtable *hashtree)
 {
-    if(current-> p_left == NULL && current-> p_right == NULL){ 		//se (o ponteiro aponta pra uma folha)
-        hashtree = putI(hashtree, current->n_character, binary_value);
-        printf("valor = %ld(%c)\n", getI(hashtree, current->n_character), current->n_character);
-        //getI(hashtree, current->n_character);
+	if(current-> p_left == NULL && current-> p_right == NULL){ 		//se (o ponteiro aponta pra uma folha)
+		hashtree = putI(hashtree, current->n_character, binary_value);
+		printf("valor = %ld(%c)\n", getI(hashtree, current->n_character), current->n_character);
+		//getI(hashtree, current->n_character);
 
-        return hashtree;
-        //hashtree = put(hashtree, current-> n_character, binary_value);	//coloca na hash a chave(caracter) e valor(binário)
-    }
+		return hashtree;
+		//hashtree = put(hashtree, current-> n_character, binary_value);	//coloca na hash a chave(caracter) e valor(binário)
+	}
 
-    else{							//se não é folha, então é representado por '*' na árvore
+	else{							//se não é folha, então é representado por '*' na árvore
 
-        //Node_String* left = buildList(left, binary_value);
-        //left = insert_node(left, '0');
-        //char* left_c = c_str(left, left_c);
-        long int left = binary_value * 10;//
-        hashtree = maptreeHashTable(current-> p_left, left, hashtree);	//percorre a árvore a esquerda e add '0'
+		//Node_String* left = buildList(left, binary_value);
+		//left = insert_node(left, '0');
+		//char* left_c = c_str(left, left_c);
+		long int left = binary_value * 10;//
+		hashtree = maptreeHashTable(current-> p_left, left, hashtree);	//percorre a árvore a esquerda e add '0'
 
-        //Node_String* right =buildList(right, binary_value);
-        //right = insert_node(right, '1');
-        //char* right_c = c_str(right, right_c);
-        long int right = binary_value * 10 + 1;
-        hashtree = maptreeHashTable(current-> p_right, right, hashtree);//percorre a árvore a direita e add '1'
-    }
-    return hashtree;
+		//Node_String* right =buildList(right, binary_value);
+		//right = insert_node(right, '1');
+		//char* right_c = c_str(right, right_c);
+		long int right = binary_value * 10 + 1;
+		hashtree = maptreeHashTable(current-> p_right, right, hashtree);//percorre a árvore a direita e add '1'
+	}
+	return hashtree;
 }
 
 int getTrash(FILE* file)
@@ -80,7 +80,7 @@ int getSizeTree(FILE* file)
 	first = first & 00011111;
 	int size = (first << 8) | second;
 
-return size;
+	return size;
 }
 
 Node_String* getTree_Representation(FILE* file, Node_String* tree, int sizeTree, int sizeName)
@@ -123,7 +123,4 @@ Node_String* getName(FILE* file, Node_String* tree, int sizeName)
 	return tree;
 }
 
-//
-// Created by alvino on 03/10/16.
-//
-
+#endif// MAPTREE_C
